12a39a22464812ea58674c27c1f2b55d2b592811
kositsyna <lylychery@mail.ru>
2024-09-09 19:26:22 +0400

    площадь прямоугольника

--- SandBox/src/main/java/Geometry.java
+++ SandBox/src/main/java/Geometry.java
@@ -1,7 +1,13 @@
 public class Geometry {
     public static void main(String[] args) {
        printSquareArea ( 7);
+       printRectangleArea(3.0, 5.0);
     }
+
+    private static void printRectangleArea(double a, double b) {
+        System.out.println("Площадь  прямоугольник со сторонами  " + a + " = " + SquareArea(a))
+    }
+
     static void printSquareArea(double a)
     {
         System.out.println("Площадь  квадрата со стороной " + a + " = " + SquareArea(a));
