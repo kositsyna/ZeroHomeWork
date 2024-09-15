aa0fb3749bc5eabfe519d01e5b28f35da3a10553
kositsyna <lylychery@mail.ru>
2024-09-15 09:59:21 +0400

    добавлена функция расчета площади прямоугольника

--- SandBox/src/main/java/Geometry.java
+++ SandBox/src/main/java/Geometry.java
@@ -2,10 +2,16 @@ public class Geometry {
     public static void main(String[] args) {
        printSquareArea ( 7);
        printRectangleArea(3.0, 5.0);
+       printRectangleArea(7.0, 9.0);
+
     }
 
     private static void printRectangleArea(double a, double b) {
-        System.out.println("Площадь  прямоугольник со сторонами  " + a + " = " + SquareArea(a))
+        System.out.println("Площадь  прямоугольник со сторонами  " + a + " и " + b + " = " + RectangleArea(a,b));
+    }
+
+    private static double RectangleArea(double a, double b) {
+        return a*b;
     }
 
     static void printSquareArea(double a)
