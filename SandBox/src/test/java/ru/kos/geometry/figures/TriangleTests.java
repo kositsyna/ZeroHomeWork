6a0dec41dd6951d798506566310d4085dbdf83a4
kositsyna <lylychery@mail.ru>
2024-09-29 20:49:31 +0400

    треугольники 3.0 =/

--- SandBox/src/test/java/ru/kos/geometry/figures/TriangleTests.java
+++ SandBox/src/test/java/ru/kos/geometry/figures/TriangleTests.java
@@ -6,44 +6,41 @@ import org.junit.jupiter.api.Test;
 
 public class TriangleTests {
     @Test
-    void CanCalculateTriArea()
-    {
+    void CanCalculateTriArea() {
         var s = new Triangle(3.0, 4.0, 5.0);
         double Result = s.TriArea();
         Assertions.assertEquals(6.0, Result);
-    };
-    @Test
-    void CanCalculateTriPerimetr()
-    {
-        Assertions.assertEquals(12.0, new Triangle(3.0,4.0,5.0).TriPerimetr());
     }
+
+    ;
+
+    @Test
+    void CanCalculateTriPerimetr() {
+        Assertions.assertEquals(12.0, new Triangle(3.0, 4.0, 5.0).TriPerimetr());
+    }
+
     @Test
     void CantCreateTriangle() {
         try {
-            new Triangle(1.0,2.0,3.0);
+            new Triangle(1.0, 2.0, 3.0);
             Assertions.fail();
-        } catch (IllegalArgumentException exception){
+        } catch (IllegalArgumentException exception) {
 
         }
     }
+
     @Test
-    void testEquality(){
-        var t1 = new Triangle(5.0,4.0,8.0);
-        var t2 = new Triangle(5.0,4.0,8.0);
-        var t3 = new Triangle(5.0,4.0,8.0);
-        Assertions.assertEquals(t1,t2);
-        Assertions.assertEquals(t1,t3);
-        Assertions.assertEquals(t3,t2);
+    void testEquality() {
+        var t1 = new Triangle(5.0, 4.0, 8.0);
+        var t2 = new Triangle(5.0, 4.0, 8.0);
+        Assertions.assertEquals(t1, t2);
     }
 
     @Test
-    void testEquality2(){
-        var t1 = new Triangle(3.0,4.0,5.0);
-        var t2 = new Triangle(4.0,5.0,3.0);
-        var t3 = new Triangle(5.0,3.0,4.0);
-        Assertions.assertEquals(t1,t2);
-        Assertions.assertEquals(t1,t3);
-        Assertions.assertEquals(t2,t3);
+    void testEquality2() {
+        var t1 = new Triangle(3.0, 4.0, 5.0);
+        var t2 = new Triangle(4.0, 5.0, 3.0);
+        Assertions.assertEquals(t1, t2);
     }
 
-    }
+}
