23eee75a72adbc820837bee7f80cf3a00af6686f
kositsyna <lylychery@mail.ru>
2024-11-03 14:19:02 +0400

    111

--- addressbook_web_test/src/test/java/GroupCreationTests.java
+++ addressbook_web_test/src/test/java/GroupCreationTests.java
@@ -1,80 +1,44 @@
-<<<<<<< HEAD
-import model.GroupData;
-=======
-<<<<<<< Updated upstream
->>>>>>> origin/main
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-=======
-<<<<<<< HEAD
-import model.GroupData;
-=======
->>>>>>> origin/main
-import org.openqa.selenium.By;
->>>>>>> origin/main
->>>>>>> Stashed changes
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 
-public class GroupCreationTests extends TestBase {
+import java.util.*;
 
-
-    @org.junit.jupiter.api.Test
-    public void canCreateGroup() {
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-        app.openGroupsPage();
-        app.createGroup(new GroupData("group name", "group header", "group footer"), GroupCreationTests.this);
-=======
->>>>>>> Stashed changes
-        openGroupsPage();
-<<<<<<< HEAD
-        createGroup(new GroupData("group name", "group header", "group footer"));
-=======
-        createGroup("group name", "group header", "group footer");
->>>>>>> origin/main
-
-<<<<<<< Updated upstream
-=======
->>>>>>> origin/main
->>>>>>> Stashed changes
-    }
-
-    @org.junit.jupiter.api.Test
-    public void canCreateGroupWithEmptyName() {
-<<<<<<< Updated upstream
-        openGroupsPage();
-<<<<<<< HEAD
-        createGroup(new GroupData());
-=======
-<<<<<<< HEAD
-        app.openGroupsPage();
-        app.createGroup(new GroupData());
-=======
-        openGroupsPage();
-<<<<<<< HEAD
-        createGroup(new GroupData());
->>>>>>> origin/main
->>>>>>> Stashed changes
-    }
-    @org.junit.jupiter.api.Test
-    public void canCreateGroupWithOnlyName() {
-        openGroupsPage();
-        var emptyGroup = new GroupData();
-        var groupWithName = emptyGroup.withName("some name");
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-        app.createGroup(groupWithName);
-=======
->>>>>>> Stashed changes
-        createGroup(groupWithName);
-=======
-        driver.findElement(By.linkText("groups")).click();
-        createGroup("", "", "");
-
->>>>>>> origin/main
-<<<<<<< Updated upstream
-=======
->>>>>>> origin/main
->>>>>>> Stashed changes
-    }
+public class GroupCreationTests {
+  private WebDriver driver;
+  private Map<String, Object> vars;
+  JavascriptExecutor js;
+  @BeforeEach
+  public void setUp() {
+    driver = new ChromeDriver();
+    js = (JavascriptExecutor) driver;
+    vars = new HashMap<String, Object>();
+  }
+  @AfterEach
+  public void tearDown() {
+    driver.quit();
+  }
+  @Test
+  public void test1() {
+    driver.get("http://localhost/addressbook/");
+    driver.manage().window().setSize(new Dimension(1386, 742));
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+    driver.findElement(By.linkText("groups")).click();
+    driver.findElement(By.name("new")).click();
+    driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).sendKeys("groupname");
+    driver.findElement(By.name("group_header")).sendKeys("groupheader");
+    driver.findElement(By.name("group_footer")).sendKeys("groupfooter");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.linkText("group page")).click();
+    driver.findElement(By.linkText("Logout")).click();
+  }
 }
